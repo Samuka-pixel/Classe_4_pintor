426f407dfe8a73f4d3ac762b6f978e72450c27c2
Samuka-pixel <shark06778@sharkcoders.pt>
2024-10-26 10:30:31 +0100

    Add files via upload

---
+++ pintor.py
@@ -0,0 +1,4 @@
+L1 = int(input("qual é a altura da tua parede?: "))         #L1 é a Altura
+L2 = int(input("qual é o comprimento da tua parede?: "))    #L2 é o comprimento
+A1 = L1 * L2                                                #A1 é a Área da parede
+print("A área da sua parede é de", A1 , "m2, e ira percisar de", A1/2, "L de tinta")
